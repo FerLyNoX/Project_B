55b22ccea3db202caf98224f26f7a966d8ce4dc5
jaksam <jaksam@ciat.net.ua>
2020-04-27 16:02:41 +0300

    добавлены фильтры  + Сделан фильр на Incomes

--- bala/views/projects.py
+++ bala/views/projects.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from bala.models import Project
+from django.db.models import Sum
+from bala.models import Project, ProjectMembers, Incomes, Outcomes
 from .urls import get_urls
 from django.urls import reverse_lazy
 from django_filters import FilterSet, DateRangeFilter, DateFromToRangeFilter
@@ -30,11 +31,11 @@ class ProjectListView(ListView):
     template_name = 'project_list.html'
 
     def get_queryset(self):
-        fff = ProjectFilter(self.request.GET, queryset=Project.objects.all())
+        fff = ProjectFilter(self.request.GET, queryset=Project.objects.order_by('id'))
         return fff.qs
 
     def get_context_data(self, *args, **kwargs):
-        fff = ProjectFilter(self.request.GET, queryset=Project.objects.all())
+        fff = ProjectFilter(self.request.GET, queryset=Project.objects.order_by('id'))
         return update_context(
             super().get_context_data(*args, **kwargs),
             filter=fff,
@@ -43,12 +44,24 @@ class ProjectListView(ListView):
 
 class ProjectEditView(UpdateView):
     model = Project
-    template_name = 'item.html'
+    template_name = 'project_item.html'
     fields = ('name', 'customer', 'area', 'cost', 'description',)
     success_url = reverse_lazy('projects')
 
     def get_context_data(self, *args, **kwargs):
-        return update_context(super().get_context_data(**kwargs))
+        instance = self.get_object()
+        # планируемые выплаты по проекту
+        outcomes_plan = ProjectMembers.objects.filter(project=instance).aggregate(sum=Sum('sum'))
+        # фактические посутпления
+        incomes_fact = Incomes.objects.filter(project=instance).aggregate(sum=Sum('sum'))
+        # фактические выплаты
+        outcomes_fact = Outcomes.objects.filter(project=instance).order_by('date').aggregate(sum=Sum('sum'))
+        return update_context(
+            super().get_context_data(**kwargs),
+            outcomes_plan=outcomes_plan,
+            incomes_fact=incomes_fact,
+            outcomes_fact=outcomes_fact,
+        )
 
 
 class ProjectCreateView(CreateView):
