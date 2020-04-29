3963c60c62fee81eef434e9626a619ec54b2eb72
jaksam <jaksam@ciat.net.ua>
2020-04-29 14:39:34 +0300

    local_settings

--- Project_B/settings.py
+++ Project_B/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = 'Project_B.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+try:
+    from .local_settings import DATABASES
+except ImportError:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
